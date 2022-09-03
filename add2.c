2b09078e13c21bb0632a95428d46aecc476e9d12
ramprasad.ap@gmail.com <ramprasad.ap@gmail.com>
2022-09-03 11:07:43 +0530

    added support for input

--- add2.c
+++ add2.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int main(int argc, char **argv) { 
+int main(int argc, char **argv)
+{
     int num1, num2;
 
-    if(argc < 3) {
-        printf("Usage:  ./add2 <num1> <num2> \n");
-        exit(1);
+    if (argc < 3)
+    {
+        printf("Enter Num1: ");
+        scanf("%d", &num1);
 
+        printf("Enter Num2: ");
+        scanf("%d", &num2);
+    }
+    else
+    {
+
+        num1 = atoi(argv[1]);
+        num2 = atoi(argv[2]);
     }
 
-
-
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
-
-    printf("Sum of %d and %d is %d\n", num1, num2,num1+num2);
+    printf("Sum of %d and %d is %d\n", num1, num2, num1 + num2);
 }
-
